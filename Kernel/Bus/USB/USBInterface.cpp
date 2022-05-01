86263bbeace8d192335f0d5edb79752e61883c60
b14ckcat <b14ckcat@protonmail.com>
2022-04-30 21:42:05 -0400

    Kernel/USB: Probe for QEMU virtual MSC device

--- Kernel/Bus/USB/USBInterface.cpp
+++ Kernel/Bus/USB/USBInterface.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, blackcat <b14ckcat@protonmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #include <Kernel/Bus/USB/USBClasses.h>
 #include <Kernel/Bus/USB/USBConfiguration.h>
 #include <Kernel/Bus/USB/USBInterface.h>
@@ -10,6 +16,14 @@ namespace Kernel::USB
 // Temporary function to load drivers for certain USB device classes
 void USBInterface::load_drivers()
 {
+    // QEMU flakiness
+    // Mass storage class workaround
+    auto m_device_descriptor = m_configuration.device().device_descriptor();
+    if (m_device_descriptor.vendor_id == USB_MSC_QEMU_VID &&
+        m_device_descriptor.product_id == USB_MSC_QEMU_PID) {
+            dbgln("Testing testing");
+	}
+
     // Mass storage class
     if (m_descriptor.interface_class_code == USB_CLASS_MASS_STORAGE &&
         m_descriptor.interface_sub_class_code == USB_MSC_SUBCLASS_SCSI_TRANSPARENT_COMMAND_SET &&
