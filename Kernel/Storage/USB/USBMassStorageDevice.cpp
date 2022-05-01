7ce9c656558554d5b8688b557d1ef8ad11f01e3d
b14ckcat <b14ckcat@protonmail.com>
2022-05-01 19:39:04 -0400

    Kernel/USB: Use real major/minor numbers for MSC device

--- Kernel/Storage/USB/USBMassStorageDevice.cpp
+++ Kernel/Storage/USB/USBMassStorageDevice.cpp
@@ -8,23 +8,24 @@
 #include <Kernel/Bus/USB/USBDevice.h>
 #include <Kernel/Devices/DeviceManagement.h>
 #include <Kernel/FileSystem/OpenFileDescription.h>
+#include <Kernel/Storage/StorageManagement.h>
 #include <Kernel/Storage/USB/USBMassStorageDevice.h>
 
 namespace Kernel {
 
 ErrorOr<NonnullRefPtr<USBMassStorageDevice>> USBMassStorageDevice::create(USB::Device const& usb_device)
 {
+    auto minor_number = StorageManagement::generate_storage_minor_number();
     auto device_name = MUST(KString::formatted("storage device"));
-    auto device_or_error = DeviceManagement::try_create_device<USBMassStorageDevice>(usb_device, move(device_name));
+    auto device_or_error = DeviceManagement::try_create_device<USBMassStorageDevice>(usb_device, minor_number, move(device_name));
     VERIFY(!device_or_error.is_error());
     return device_or_error.release_value();
 }
 
-USBMassStorageDevice::USBMassStorageDevice(USB::Device const& usb_device, NonnullOwnPtr<KString> device_name)
-    : StorageDevice(0x13, 0x37, 512, 512*8, move(device_name))
+USBMassStorageDevice::USBMassStorageDevice(USB::Device const& usb_device, MinorNumber minor_number, NonnullOwnPtr<KString> device_name)
+    : StorageDevice(StorageManagement::storage_type_major_number(), minor_number, 512, 512*8, move(device_name))
     , m_attached_usb_device(usb_device)
 {
-    
 }
 
 USBMassStorageDevice::~USBMassStorageDevice() = default;
