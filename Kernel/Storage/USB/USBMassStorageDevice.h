40d0a7fbb3176efa121f81e2169be48096d000cc
b14ckcat <b14ckcat@protonmail.com>
2022-05-15 15:33:00 -0400

    Use correct terminology

--- Kernel/Storage/USB/USBMassStorageDevice.h
+++ Kernel/Storage/USB/USBMassStorageDevice.h
@@ -19,8 +19,8 @@ struct SCSIMetadata {
     u8 max_lun;
     unsigned char vendor_id[8];
     unsigned char product_id[16];
-    u32 sector_size;
-    u32 num_sectors;
+    u16 block_size;
+    u32 num_blocks;
     bool read_only;
 };
 
@@ -41,8 +41,8 @@ private:
 
     static ErrorOr<OwnPtr<SCSIMetadata>> get_metadata(OwnPtr<USB::MassStorageHandle> &usb_msc_handle);
 
-    ErrorOr<void> read(u32 lba, u16 len, void * buf);
-    ErrorOr<void> write(u32 lba, u16 len, void * buf);
+    ErrorOr<void> read(u32 lba, void * buf);
+    ErrorOr<void> write(u32 lba, void * buf);
 
     OwnPtr<SCSIMetadata> m_metadata;
     OwnPtr<USB::MassStorageHandle> m_usb_msc_handle;
