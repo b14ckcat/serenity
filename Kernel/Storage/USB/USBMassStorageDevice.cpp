6f024922e4067c6fb05e1477a83083db890e41fe
b14ckcat <b14ckcat@protonmail.com>
2022-05-15 15:33:00 -0400

    Use correct terminology

--- Kernel/Storage/USB/USBMassStorageDevice.cpp
+++ Kernel/Storage/USB/USBMassStorageDevice.cpp
@@ -34,14 +34,14 @@ ErrorOr<NonnullRefPtr<USBMassStorageDevice>> USBMassStorageDevice::create(OwnPtr
 }
 
 USBMassStorageDevice::USBMassStorageDevice(OwnPtr<USB::MassStorageHandle> usb_msc_handle, MinorNumber minor_number, OwnPtr<SCSIMetadata> metadata, NonnullOwnPtr<KString> device_name)
-    : StorageDevice(StorageManagement::storage_type_major_number(), minor_number, metadata->sector_size, metadata->num_sectors, move(device_name))
+    : StorageDevice(StorageManagement::storage_type_major_number(), minor_number, metadata->block_size, metadata->num_blocks, move(device_name))
     , m_metadata(move(metadata))
     , m_usb_msc_handle(move(usb_msc_handle))
 {
-    u8 buf[1024] = {'\0'};
+    u8 buf[512] = {'\0'};
     u8 print[17] = {'\0'};
     for (int j = 0; j < 64; j++) {
-        dbgln("{}", read(1024*j, 1024, buf).is_error());
+        dbgln("{}", read(0, buf).is_error());
         for (int i = 0; i < 64; i++) {
             memcpy(print, buf+(i*16), 16);
             dbgln("{}", print);
@@ -85,11 +85,11 @@ ErrorOr<OwnPtr<SCSIMetadata>> USBMassStorageDevice::get_metadata(OwnPtr<USB::Mas
     memcpy(metadata->vendor_id, inq_res.vendor_id, 8);
     memcpy(metadata->product_id, inq_res.product_id, 16);
 
-    // Read capacity = get sector size & number of sectors on device
+    // Read capacity = get block size & number of blocks on device
     ReadCapacityResponse cap_res;
     TRY(usb_msc_handle->try_scsi_command<CommandDescriptorBlock10>(CDB_READ_CAPACITY, 0, Pipe::Direction::In, sizeof(ReadCapacityResponse), &cap_res));
-    metadata->sector_size = AK::convert_between_host_and_big_endian(cap_res.sector_size);
-    metadata->num_sectors = AK::convert_between_host_and_big_endian(cap_res.num_sectors);
+    metadata->block_size = AK::convert_between_host_and_big_endian(cap_res.block_size);
+    metadata->num_blocks = AK::convert_between_host_and_big_endian(cap_res.num_blocks);
 
     // Determine if device is read-only
     u8 page[MODE_SENSE_PAGE_LEN];
@@ -99,34 +99,34 @@ ErrorOr<OwnPtr<SCSIMetadata>> USBMassStorageDevice::get_metadata(OwnPtr<USB::Mas
     return metadata;
 }
 
-ErrorOr<void> USBMassStorageDevice::read(u32 lba, u16 len, void * buf)
+ErrorOr<void> USBMassStorageDevice::read(u32 lba, void * buf)
 {
     CommandDescriptorBlock10 cdb_read = {
         .opcode = SCSI_READ_10,
         .misc_and_service = 0x00,
         .logical_block_addr = lba,
         .misc_continued = 0x00,
-        .len = len,
+        .len = m_metadata->block_size,
         .control = 0x00
     };
 
-    TRY(m_usb_msc_handle->try_scsi_command<CommandDescriptorBlock10>(cdb_read, 0, Pipe::Direction::In, len, buf));
+    TRY(m_usb_msc_handle->try_scsi_command<CommandDescriptorBlock10>(cdb_read, 0, Pipe::Direction::In, m_metadata->block_size, buf));
 
     return {};
 }
 
-ErrorOr<void> USBMassStorageDevice::write(u32 lba, u16 len, void * buf)
+ErrorOr<void> USBMassStorageDevice::write(u32 lba, void * buf)
 {
     CommandDescriptorBlock10 cdb_write = {
         .opcode = SCSI_WRITE_10,
         .misc_and_service = 0x00,
         .logical_block_addr = lba,
         .misc_continued = 0x00,
-        .len = len,
+        .len = m_metadata->block_size,
         .control = 0x00
     };
 
-    TRY(m_usb_msc_handle->try_scsi_command<CommandDescriptorBlock10>(cdb_write, 0, Pipe::Direction::Out, len, buf));
+    TRY(m_usb_msc_handle->try_scsi_command<CommandDescriptorBlock10>(cdb_write, 0, Pipe::Direction::Out, m_metadata->block_size, buf));
 
     return {};
 }
