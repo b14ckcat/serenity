3a35819a8f1cdcfde456595d92a0c3270711c800
Jesse Buhagiar <jooster669@gmail.com>
2022-05-20 23:00:04 +1000

    Kernel/USB: Add supported USB Device table to `Driver`

--- Kernel/Bus/USB/Drivers/USBDriver.h
+++ Kernel/Bus/USB/Drivers/USBDriver.h
@@ -16,8 +16,17 @@ typedef void (*driver_init_function_t)();
 #define USB_DEVICE_DRIVER(driver_name) \
     driver_init_function_t driver_init_function_ptr __attribute__((section(".driver_init"), unused)) = &driver_name::init
 
+#define USB_DEVICE_TABLE_IDENTIFIER(vid, pid) { vid, pid },
+
 class Driver : public RefCounted<Driver> {
 public:
+    struct DeviceId {
+        u16 vendor_id;
+        u16 product_id;
+
+        bool operator==(DeviceId const& rhs) const { return ((rhs.vendor_id == vendor_id) && (rhs.product_id == product_id)); }
+    };
+
     Driver(StringView const& name)
         : m_driver_name(name)
     {
@@ -29,7 +38,10 @@ public:
     virtual void disconnect() = 0;
     virtual StringView const& name() const = 0;
 
+    Vector<DeviceId> const& device_table() const { return m_supported_device_table; }
+
 protected:
     StringView const m_driver_name;
+    Vector<DeviceId> m_supported_device_table;
 };
 }
