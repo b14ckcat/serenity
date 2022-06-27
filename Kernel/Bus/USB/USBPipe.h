e38e8435ae9d94916aa59483be3124c66e180d58
b14ckcat <b14ckcat@protonmail.com>
2022-06-27 19:17:45 -0400

    Add mutex to pipe for buffer, may or may not keep

--- Kernel/Bus/USB/USBPipe.h
+++ Kernel/Bus/USB/USBPipe.h
@@ -8,6 +8,7 @@
 
 #include <AK/OwnPtr.h>
 #include <AK/Types.h>
+#include <Kernel/Locking/Mutex.h>
 #include <Kernel/Bus/USB/USBDescriptors.h>
 #include <Kernel/Locking/Mutex.h>
 #include <Kernel/Memory/Region.h>
@@ -68,6 +69,8 @@ private:
     friend class Device;
 
     NonnullLockRefPtr<USBController> m_controller;
+
+    Mutex m_dma_buffer_lock {"USB pipe DMA buffer"};
 
     Type m_type;
     Direction m_direction;
